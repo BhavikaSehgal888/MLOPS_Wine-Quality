3f0b3e204dc2cbabd1852d76069b8f97d46318ea
Sehgal <bhavika.sehgal@accenture.com>
2021-04-24 17:32:23 +0530

    Removed data from data/raw folder

--- src/get_data.py
+++ src/get_data.py
@@ -8,15 +8,15 @@ import numpy as np
 import argparse
 
 def read_params(config_path):
-    with open(os.path.join(os.path.split(os.getcwd())[0],config_path)) as file:
+    with open(config_path) as file:
         config = yaml.safe_load(file)
     return config
 
-def get_data(config_path):
+def get_data_given(config_path):
     config = read_params(config_path)
     #print(config)
     data_path = config["data_source"]["s3_source"]
-    df = pd.read_csv(os.path.join(os.path.split(os.getcwd())[0],data_path),sep=";",encoding="utf-8")
+    df = pd.read_csv(data_path,sep=";",encoding="utf-8")
     return df
 
 
@@ -24,4 +24,4 @@ if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config",default="params.yaml")
     parse_args = args.parse_args()
-    data = get_data(config_path=parse_args.config)
+    data = get_data_given(config_path=parse_args.config)
--- src/load_data.py
+++ src/load_data.py
@@ -2,15 +2,29 @@
 # Save it in data/raw for further processing
 
 import os
-from src.get_data import read_params,get_data
+import pandas as pd
+import yaml
 import argparse
+
+
+def read_params(config_path):
+    with open(config_path) as file:
+        config = yaml.safe_load(file)
+    return config
+
+def get_data_given(config_path):
+    config = read_params(config_path)
+    #print(config)
+    data_path = config["data_source"]["s3_source"]
+    df = pd.read_csv(data_path,sep=";",encoding="utf-8")
+    return df
 
 def load_and_save_data(config_path):
     config = read_params(config_path)
-    df = get_data(config_path)
+    df = get_data_given(config_path)
     new_columns = [col.replace(" ","_") for col in df.columns]
     raw_data_path = config["load_data"]["raw_dataset_csv"]
-    df.to_csv(os.path.join(os.path.split(os.getcwd())[0],raw_data_path),sep=",",index=False, header=new_columns)
+    df.to_csv(raw_data_path,sep=",",index=False, header=new_columns)
 
 
 if __name__=="__main__":
